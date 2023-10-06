73674b6e724ff3d303c32f694fd0db56e204806e
Sienna Lloyd <sienna@linux.com>
2023-10-06 00:24:28 -0600

    fixed ci bug
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-cat << EOF >> "$HOME"/.cargo/config.toml
+cat << EOF > "$HOME"/.cargo/config.toml
 [registries.gitea]
 index = "sparse+https://git.r3t.io/api/packages/nova-engine/cargo/"
 EOF
