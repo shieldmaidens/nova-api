a26f08c85e9bf65c16331d7f78fee972130b6d1b
Sienna Lloyd <sienna@r3t.io>
2023-11-04 15:27:46 -0600

    tmp updating publish
    Change-Id: Ic87c34dfafcc610ae3fae44301e563f7bef70aa6
    Signed-off-by: Sienna Lloyd <sienna@r3t.io>

--- publish.sh
+++ publish.sh
@@ -1,11 +1,1 @@
 #! /bin/sh
-
-cat << EOF > "$HOME"/.cargo/config.toml
-[registries.gitea]
-index = "sparse+https://git.r3t.io/api/packages/nova-engine/cargo/"
-EOF
-
-cd rust || exit
-
-cargo login --registry gitea "${CARGO_REPOSITORIES_GITEA_TOKEN}"
-cargo publish --registry gitea
